29615e279deae90b99dec1e63d7b3bbe6227e129
Stenio das Neves Silva <stenionevessilva@gmail.com>
2019-08-06 19:48:46 -0300

    Adcionado o modulo 'CORS'=>> para o servidor aceitar conexões de qualquer lugar

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose =require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server =express();
 mongoose.connect('mongodb+srv://@cluster0-ep2yc.mongodb.net/dev1?retryWrites=true&w=majority',{useNewUrlParser: true});
 
+//Aceitar qualquer conexão com esse servidor
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
